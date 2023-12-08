511073b0b052a9b17595ab633b238d9a4eb5a193
Mustafa Hussain <51909599+chemical-son@users.noreply.github.com>
2023-12-08 08:56:33 +0200

    initial commit

---
+++ social_app/urls.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth.models import User
+
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representaion.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+# wire up our API  using automaitc URL routing.
+# Additonally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
