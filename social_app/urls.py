658b7897b3ca8250244ccb430daa020d59672c21
Mustafa Hussain <51909599+chemical-son@users.noreply.github.com>
2023-12-09 06:32:17 +0200

    create simple api using django-restful

--- social_app/urls.py
+++ social_app/urls.py
@@ -1,29 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth.models import User
+from rest_framework import routers
 
-from rest_framework import routers, serializers, viewsets
+from social_app.quickstart import views
 
-# Serializers define the API representaion.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 # wire up our API  using automaitc URL routing.
 # Additonally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
+
+urlpatterns += router.urls
